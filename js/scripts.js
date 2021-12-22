20e60d73efe12a18c82b6a79ae596bd7435dbc28
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2021-12-22 12:14:17 +0300

    jquery continued

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,31 @@
 $(document).ready(function () {
     $('h1').click(function () {
-        alert('This is a Header');
+        $('img').fadeOut(4000, function () {
+            $('#p1').text('Our cow has disappeared');
+            $('#p2').text('Our Goat has also Disappeared');
+        });
     });
 
     $('.clickable').click(function () {
         //alert('This is a paragraph');
-       // $('img').show();
-       $('.cow-showing').toggle();
-       $('.cow-hidden').toggle();
-       // $(this).text('This is a beautiful Cow!');
+        // $('img').show();
+        $('.cow-showing').toggle();
+        $('.cow-hidden').toggle();
+        // $(this).text('This is a beautiful Cow!');
     });
 
     $('img').click(function () {
         //$(this).hide();
-       // $('#p1').text('Our Cow has disappeared.');
+        // $('#p1').text('Our Cow has disappeared.');
         //$('#p2').text('There is no cow: ');
+        $('.cow-showing').fadeToggle();
+        $('.cow-hidden').fadeToggle();
+        $('.cow').fadeToggle();
+    });
+
+    // Button Scripts
+    $('button').click(function () {
+        $('body').addClass('redb');
     });
 })
+
